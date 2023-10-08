61df7fc5592d0d10912cf681a336de1e0cd49c45
Mark Nottingham <mnot@mnot.net>
2023-10-09 09:42:19 +1100

    Rough in

---
+++ proxy-tests/server.py
@@ -0,0 +1,14 @@
+
+
+from proxy_tests.tests import tests
+
+
+Class ProxyTestServer:
+    def __init__(self):
+        self.tests_by_id =
+
+    def start(self):
+        pass
+
+    def handle_request(self):
+        pass
